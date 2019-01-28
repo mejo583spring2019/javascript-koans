e49fc548a9027cc7ae56526f502711a902e08b8c
“SivonneWard” <sivonne@live.unc.edu>
2019-01-27 23:03:38 -0500

    finished 'should have the bomb' in 'Thinking about Objects'

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -44,8 +44,8 @@ describe("About Objects", function () {
       }
     };
 
-    expect(currentYear).toBe(FILL_ME_IN);
-    expect(megalomaniac.calculateAge()).toBe(FILL_ME_IN);
+    expect(currentYear).toBe(2019);
+    expect(megalomaniac.calculateAge()).toBe(2019-1970);
   });
 
   describe("'in' keyword", function () {
@@ -62,7 +62,7 @@ describe("About Objects", function () {
 
       var hasBomb = "theBomb" in megalomaniac;
 
-      expect(hasBomb).toBe(FILL_ME_IN);
+      expect(hasBomb).toBe(true);
     });
 
     it("should not have the detonator however", function () {
